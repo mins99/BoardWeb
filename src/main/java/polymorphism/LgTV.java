93754f5efce98245ac34c987e4d5efb681333920
Celina KIM <luv.mins99@gmail.com>
2019-01-13 20:18:57 +0900

    * DAY1 CLASS5 5.2.4

--- src/main/java/polymorphism/LgTV.java
+++ src/main/java/polymorphism/LgTV.java
@@ -1,26 +1,34 @@
 package polymorphism;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("tv")
 public class LgTV implements TV{
+	@Autowired
+	@Qualifier("apple")	// 동일 type autowired시 구분하기 위해 사용
+	/*@Resource(name="apple")*/
+	private Speaker speaker;
 	public LgTV() {
 		System.out.println("===> LgTV 객체 생성");
 	}
 	
 	public void powerOn() {
-		System.out.println("LgTV---���� �Ҵ�.");
+		System.out.println("LgTV---전원 켠다.");
 	}
 
 	public void powerOff() {
-		System.out.println("LgTV---���� ����.");
+		System.out.println("LgTV---전원 끈다.");
 	}
 
 	public void volumeUp() {
-		System.out.println("LgTV---�Ҹ� �ø���.");
+		speaker.volumeUp();
 	}
 
 	public void volumeDown() {
-		System.out.println("LgTV---�Ҹ� ������.");
+		speaker.volumeDown();
 	}
 }
